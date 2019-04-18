c6dd09ac37d196bd00f816045161ed05e050d03b
Nikhilydv <43957720+Nikhilydv@users.noreply.github.com>
2019-04-18 23:58:41 +0530

    DataTypes

---
+++ Question16.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+int main()
+{
+int n;
+printf("\t\t\t\t\tENTER THE NUMBER OF PROCESSES \n\t\t\t\t\t");
+ scanf("%d",&n);
+ int p;
+ p=n;
+ int ts;
+ 
+}         
+

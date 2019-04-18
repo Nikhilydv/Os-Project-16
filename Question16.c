100984c856921593844b42c384544181bdaf223f
Nikhilydv <43957720+Nikhilydv@users.noreply.github.com>
2019-04-19 00:01:55 +0530

    Round Robin Algorithm

--- Question16.c
+++ Question16.c
@@ -9,7 +9,7 @@ printf("\t\t\t\t\tENTER THE NUMBER OF PROCESSES \n\t\t\t\t\t");
  int p;
  p=n;
  int ts;
-  printf("\n\t\t\t\t\tEnter the Timeslice\n\t\t\t\t\t");
+ printf("\n\t\t\t\t\tEnter the Timeslice\n\t\t\t\t\t");
  scanf("%d",&ts);
 system("cls");
  int p_id[10],bt[10],bt1[10];
@@ -23,7 +23,7 @@ scanf("%d",&bt[i]);
 bt1[i]=bt[i];
 system("cls");
 }
- int wt[10];
+int wt[10];
 int flag[10];
 for(i=1;i<=n;i++)
 {
@@ -62,5 +62,24 @@ flag[i]=0;
 }
  }
 }
+int tat[10],totwt=0,totat=0;
+for(i=1;i<=p;i++)
+{
+tat[i]=wt[i]+bt[i];
+totwt=totwt+wt[i];
+totat=totat+tat[i];
+}
+float avgwt,avgat;
+avgwt=(float)totwt/p;
+avgat=(float)totat/p;
+printf("\n\n \t\t\t\tROUND ROBIN SCHEDULING ALGORITHM \n\n");
+printf("\n\n\t PROCESS \t PROCESS ID  \t BURST TIME \t WAITING TIME \t TURNAROUND TIME \n ");
+for(i=1;i<=p;i++)
+{
+printf("\n\t %5d \t\t %5d \t\t %5d \t\t %5d \t\t %5d \n", i,p_id[i],bt[i],wt[i],tat[i]);
+}
+printf("\n\n \t\t\t\tAVERAGE WAITING TIME\t=%.2f",avgwt);
+printf("\n\n \t\t\t\tAVERAGE TURN AROUND TIME=%.2f",avgat);
+getch();
 }         
 
